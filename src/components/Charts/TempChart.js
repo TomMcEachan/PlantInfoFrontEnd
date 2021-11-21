154ae7b44f68a2e26c26d69579aeb6668574579b
Thomas McEachan <Tom.a.mceachan@outlook.com>
2021-11-21 15:45:22 +0000

    Removed tooltip

--- src/components/Charts/TempChart.js
+++ src/components/Charts/TempChart.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-import { VictoryLine, VictoryBar, VictoryChart, VictoryTheme , VictoryAxis, VictoryTooltip} from 'victory';
+import { VictoryLine, VictoryBar, VictoryChart, VictoryTheme , VictoryAxis} from 'victory';
 
 
 export default function TempChart() {
